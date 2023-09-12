88de2fc469cd401b4e6a03e7acb526a8cba1a553
biasvestka <biasvestka@gmail.com>
2023-09-12 14:01:57 -0300

    att

--- at_react/src/App.jsx
+++ at_react/src/App.jsx
@@ -1,26 +1,18 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Header from './components/Header';
-import Footer from './components/Footer';
 import Home from './components/Home';
 import Aparelhos from './components/Aparelhos';
 import VisualizarAparelho from './components/VisualizarAparelho';
 import NotFound from './components/NotFound';
+import { Outlet } from 'react-router-dom';
+import Header from './components/Header';
+
 
 function App() {
   return (
-    <Router>
-      <div>
-        <Header />
-        <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/aparelhos" exact component={Aparelhos} />
-          <Route path="/aparelhos/:id" component={VisualizarAparelho} />
-          <Route component={NotFound} />
-        </Switch>
-        <Footer />
-      </div>
-    </Router>
+    <>
+    <Header></Header>
+    </>
   );
 }
 
--- at_react/src/main.jsx
+++ at_react/src/main.jsx
@@ -1,10 +1,34 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import App from './App'
+import NotFound from './components/NotFound'
+import Home from './components/Home'
+import Aparelhos from './components/Aparelhos'
+import VisualizarAparelho from './components/VisualizarAparelho'
 
+const router = createBrowserRouter()[{
+  path: "/", 
+  element:<App></App>,
+  errorElement: <NotFound></NotFound>,
+  children: [
+    {
+      path: "/",
+      element:<Home></Home> 
+    },
+    {
+      path: "/Aparelhos",
+      element:<Aparelhos></Aparelhos>
+    },
+    {
+      path: "/VizualizarAparelho",
+      element:<VisualizarAparelho></VisualizarAparelho>
+    }
+  ]
+}]
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
