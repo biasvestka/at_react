e30b5f67762b79ba9b7fdfd1941c9eaee459a7f8
biasvestka <biasvestka@gmail.com>
2023-09-12 12:31:04 -0300

    Lista de aparelhos criada

---
+++ at_react/src/components/Aparelhos.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import aparelhosData from '../data/aparelhos';
+
+const Aparelhos = () => {
+  return (
+    <div>
+      <h2>Aparelhos Disponíveis</h2>
+      {/* Renderize a lista de aparelhos a partir dos dados em aparelhosData */}
+    </div>
+  );
+};
+
+export default Aparelhos;
---
+++ at_react/src/data/aparelhos.js
@@ -0,0 +1,34 @@
+const aparelhos = [
+    {
+      id: '1',
+      nome: 'iPhone 12 Pro',
+      imagem: './assets/iphone12pro.jpg',
+      descricao: 'O iPhone 12 Pro é o ápice da inovação da Apple. Com sua tela Super Retina XDR, poder de processamento incrível e sistema de câmera tripla, este smartphone redefine a excelência.',
+      preco: 1199.99,
+    },
+    {
+      id: '2',
+      nome: 'Samsung Galaxy S21',
+      imagem: './assests/galaxys21.jpg',
+      descricao: 'O Samsung Galaxy S21 oferece um desempenho excepcional, uma tela AMOLED incrível e uma câmera versátil. É a escolha perfeita para quem busca um smartphone de alto nível.',
+      preco: 999.99,
+    
+    },
+    {
+      id: '3',
+      nome: 'Motorola Moto G7',
+      imagem: './assets/motog7.jpg',
+      descricao: 'O Motorola Moto G7 é um smartphone acessível com ótimo custo-benefício. Com sua tela grande e desempenho sólido, ele atende às necessidades do dia a dia.',
+      preco: 299.99,
+    },
+    {
+      id: '4',
+      nome: 'Samsung Galaxy Tab A',
+      imagem: './assets/galaxytab.jpg',
+      descricao: 'O Samsung Galaxy Tab A é um tablet versátil para entretenimento e produtividade. Sua tela de alta resolução e desempenho confiável fazem dele uma ótima escolha.',
+      preco: 249.99,
+    },
+  ];
+  
+  export default aparelhos;
+  
